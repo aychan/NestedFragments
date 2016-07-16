fa0f00f8f12d4d47f116bc24f08636aae6598eec
Anthony Chan <anthony.me.chan@gmail.com>
2016-07-16 14:53:25 -0400

    change user email, so not linked to umass email account

--- app/src/main/java/com/se/aychan/nestedfragments/MainActivity.java
+++ app/src/main/java/com/se/aychan/nestedfragments/MainActivity.java
@@ -18,7 +18,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+//new email should be linked to this commit, for aychan user
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
